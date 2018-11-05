a32e6129a91ac296e7cee6fbfac7de2cd894df41
Lauren Ferreira <lauren.@gmail.com>
2018-11-05 11:37:33 -0200

    Changing router responsability to anothr package

--- rank/main_test.go
+++ rank/main_test.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/gin-gonic/gin"
 )
 
 func TestMainExecution(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 	go main()
 }
 
-func TestRouter(t *testing.T) {
-	router := setupRouter()
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/hello", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "Hello, Rank!", w.Body.String())
+func TestRankExecution(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	go Rank()
 }
