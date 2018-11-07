c2d2ba0b429b391b542ec0a292948e69156810fd
Lauren Ferreira <lauren.@gmail.com>
2018-11-07 18:31:10 -0200

    Common tests

---
+++ rank/controller/common_test.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"log"
+	"testing"
+
+	"github.com/juju/mgosession"
+	"github.com/stretchr/testify/assert"
+	"github.coventry.ac.uk/340CT-1819SEPJAN/ferrei28-server-side/rank/framework/config"
+	"github.coventry.ac.uk/340CT-1819SEPJAN/ferrei28-server-side/rank/repository"
+	mgo "gopkg.in/mgo.v2"
+)
+
+func TestNew(t *testing.T) {
+	session, err := mgo.Dial(config.MONGODB_HOST)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer session.Close()
+
+	pool := mgosession.NewPool(nil, session, config.MONGODB_CONNECTION_POOL)
+	defer pool.Close()
+
+	repo := repository.NewMongoConnection(pool, config.MONGODB_DATABASE)
+
+	t.Run("should have created Review controller", func(t *testing.T) {
+
+		controllers := New(repo)
+		assert.NotNil(t, controllers.Review)
+	})
+}
