bf773aa03edcd5b6aebddf6c874fba31c2edce9f
Lauren Ferreira <lauren.@gmail.com>
2018-11-07 13:46:42 -0200

    Refactoring Review repository

--- rank/repository/review.go
+++ rank/repository/review.go
@@ -2,20 +2,23 @@ package repository
 
 import (
 	"github.coventry.ac.uk/340CT-1819SEPJAN/ferrei28-server-side/rank/entity"
+	"github.coventry.ac.uk/340CT-1819SEPJAN/ferrei28-server-side/rank/framework/config"
 	"github.coventry.ac.uk/340CT-1819SEPJAN/ferrei28-server-side/rank/util"
 	mgo "gopkg.in/mgo.v2"
 )
 
-type ReviewRepository interface {
+// Review defines the methods must be implemented by injected layer.
+type Review interface {
 	FindAll() ([]*entity.Review, error)
 	Store(*entity.Review) (util.Identifier, error)
 }
 
+// FindAll returns all Reviews from the database sorted by ID.
 func (m *MongoConn) FindAll() ([]*entity.Review, error) {
 	var reviews []*entity.Review
 
 	session := m.pool.Session(nil)
-	collection := session.DB(m.db).C("reviews")
+	collection := session.DB(m.db).C(config.REVIEW_COLLECTION)
 	err := collection.Find(nil).Sort("id").All(&reviews)
 	switch err {
 	case nil:
@@ -27,12 +30,15 @@ func (m *MongoConn) FindAll() ([]*entity.Review, error) {
 	}
 }
 
-func (r *MongoConn) Store(v *entity.Review) (util.Identifier, error) {
-	session := r.pool.Session(nil)
-	coll := session.DB(r.db).C("review")
-	err := coll.Insert(v)
+// Store inserts a new Review in the database.
+func (m *MongoConn) Store(review *entity.Review) (util.Identifier, error) {
+	session := m.pool.Session(nil)
+	coll := session.DB(m.db).C(config.REVIEW_COLLECTION)
+
+	review.ID = util.NewID()
+	err := coll.Insert(review)
 	if err != nil {
 		return "", err
 	}
-	return v.ID, nil
+	return review.ID, nil
 }
