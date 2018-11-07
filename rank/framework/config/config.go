facc440f2dbcb9fb6d77a7d2337d331594590052
Lauren Ferreira <lauren.@gmail.com>
2018-11-07 13:27:46 -0200

    Defining new HOST and adding REVIEW_COLLECTION

--- rank/framework/config/config.go
+++ rank/framework/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 const (
-	MONGODB_HOST            = "mongodb://127.0.0.1:27017"
+	MONGODB_HOST            = "mongodb://ferrei28:ferrei28@ds153763.mlab.com:53763/rank"
 	MONGODB_DATABASE        = "rank"
 	MONGODB_CONNECTION_POOL = 50
+	REVIEW_COLLECTION       = "review"
 	API_PORT                = 8080
 )
