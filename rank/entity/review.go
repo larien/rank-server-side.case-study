0b976820c9c8320ba10c6e74ae5b23c871bc4add
Lauren Ferreira <lauren.@gmail.com>
2018-12-05 10:25:23 -0200

    Rating entity created

--- rank/entity/review.go
+++ rank/entity/review.go
@@ -11,11 +11,18 @@ type Review struct {
 	ID              util.Identifier `bson:"_id,omitempty" json:"id"`
 	Title           string          `bson:"title" json:"title"`
 	AuthorNickname  string          `bson:"author_nickname" json:"author_nickname"`
-	AverageRating   int             `bson:"average_rating" json:"average_rating"`
 	PublicationDate time.Time       `bson:"publication_date" json:"publication_date"`
 	UpdatedAt       time.Time       `bson:"updated_at" json:"updated_at"`
 	ReadingTime     string          `bson:"reading_time" json:"reading_time"`
 	TextReview      string          `bson:"text_review" json:"text_review"`
 	CoverImage      string          `bson:"cover_image" json:"cover_image"`
 	IsPublished     bool            `bson:"is_published" json:"is_published"`
+}
+
+// Rating represents the rating entity and its atttributes.
+type Rating struct {
+	ID       util.Identifier `bson:"_id,omitempty" json:"id"`
+	ReviewID util.Identifier `bson:"review_id,omitempty" json:"review_id"`
+	// UserID   util.Identifier `bson:"user_id,omitempty" json:"user_id"`
+	Rating int `bson:"rating" json:"rating"`
 }
